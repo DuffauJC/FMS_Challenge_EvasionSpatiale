b2aa739a6ab95350d791eae8678ce90565664d63
DuffauJC <jeancharles.duffau@gmail.com>
2022-04-07 11:47:23 +0200

    v3 ajout du gif animé

--- src/fr/fms/graphic/GraphicSpace.java
+++ src/fr/fms/graphic/GraphicSpace.java
@@ -1,42 +1,42 @@
 package fr.fms.graphic;
 
-import java.awt.Graphics;
-import java.awt.HeadlessException;
-import java.util.HashMap;
-import java.util.Map;
-
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 
 public class GraphicSpace extends JFrame {
 	private static final long serialVersionUID = 1L;
 
-
-
+	  JPanel contentPane;
+	    JLabel imageLabel = new JLabel();
+	    JLabel headerLabel = new JLabel();
 
 	public GraphicSpace() {
-		super("Voici nos formes g�om�triques !");
+		super();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(350, 250);
 		setLocationRelativeTo(null);
 		setVisible(true);
-	}
-
-	@Override
-	public void paint(Graphics g) { // le rep�re x,y commence en haut � gauche (0,0)
-		super.paint(g);
-
-
-		g.fillOval(50, 50, 35, 35);	// x , y , largeur , hauteur (diam�tre)
-//		g.drawOval(150, 50, 45, 70);
-//		g.drawRect(50, 150, 50, 50);
-//		g.fillRect(150, 150, 70, 50);		
-
-//		Graphics2D g2d = (Graphics2D)g;
-//		g2d.setPaint( new GradientPaint(0, 0, Color.GREEN, 50, 50, Color.RED, true));
-//		g2d.fillRect(250, 50, 70, 50);		
-//		
-//		g2d.fillOval(250,150,55,55);		
+		 try {
+	            setDefaultCloseOperation(EXIT_ON_CLOSE);
+	            contentPane = (JPanel) getContentPane();
+	           
+	            setTitle("Your Job Crashed!");
+	            // add the header label
+	         
+	            contentPane.add(headerLabel, java.awt.BorderLayout.NORTH);
+	            // add the image label
+	            ImageIcon ii = new ImageIcon( "Yoda_cat.gif");
+	                   
+	            imageLabel.setIcon(ii);
+	            contentPane.add(imageLabel, java.awt.BorderLayout.CENTER);
+	            // show it
+	            this.setVisible(true);
+	        } catch (Exception exception) {
+	            exception.printStackTrace();
+	        }
 	}
 
 	public static void main(String[] args) {
--- src/fr/fms/thread/ThreadTime.java
+++ src/fr/fms/thread/ThreadTime.java
@@ -1,32 +1,22 @@
 package fr.fms.thread;
 
-import java.text.DateFormat;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import fr.fms.graphic.GraphicSpace;
 
 public class ThreadTime {
 
 	public static void main(String[] args) {
 
-		DateFormat df = new SimpleDateFormat("HH:mm:ss");
-		Thread thread = new Thread(new MonRunnable(5000, df)); //
-
-		//System.out.println(df.format(new Date()));
-
+		Thread thread = new Thread(new MonRunnable(5000)); //
 		thread.start();
 	}
 
 	public static class MonRunnable implements Runnable {
 
 		private long delai;
-		private DateFormat df; //
 
-		public MonRunnable(long delai, DateFormat df) { //
+		public MonRunnable(long delai) { //
 			this.delai = delai;
-			this.df = df; //
+			
 		}
 
 		@Override
